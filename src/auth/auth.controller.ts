a03084aeb6f2cb6265335f5717f03ea67396c72b
Yeison Alexis Velasco Trejos <yeison.velasco@caminos.com.co>
2024-01-03 12:24:37 -0500

    register y login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,8 +10,11 @@ export class AuthController {
     ){}
 
     @Post('login')
-    login(){
-        return this.authService.login();
+    login(
+        @Body()
+        loginDto:LoginDto
+    ){
+        return this.authService.login(loginDto);
     }    
 
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,35 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+const bcrypt = require("bcryptjs");
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UsersService){}
     
-    login(){
-        return "login";
+    async login({Email,Password}:LoginDto){
+        const user =  await this.userService.finfOneByEmail(Email)
+        if(!user){
+            throw new UnauthorizedException('No se encuentra registrado');
+        }
+
+        const isPasswordValid = await bcrypt.compare(Password,user.Password)
+        if(!isPasswordValid){
+            throw new UnauthorizedException('La contraseña está incorrecta'); 
+        }
+        return user;
     }    
 
-    register(registerDto: RegisterDto){
-        
-        
-        const user = this.userService.finfOneByEmail(registerDto.Email)
+    async register(registerDto: RegisterDto){  
+        const user =  await this.userService.finfOneByEmail(registerDto.Email)
+        registerDto.Password = await bcrypt.hash(registerDto.Password,10)
         if(user){
             throw new BadRequestException("El Usuario ya se encuentra registrado")
         }
 
         return this.userService.create(registerDto)
     }
+
 
 }
